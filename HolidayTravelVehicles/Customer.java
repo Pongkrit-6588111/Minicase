899046bb19008c474731815fb91ef9b48ee7bddc
PONGKRIT UBONTAM <pongkrit.ubo@student.mahidol.edu>
2025-02-27 22:12:31 +0700

    delete class from main

--- HolidayTravelVehicles/Customer.java
+++
@@ -1,6 +0,0 @@
-// ----------------------- Customer Class (Inherits from Person) ----------------------- //
-class Customer extends Person {
-    public Customer(String name, String address, String phoneNumber) {
-        super(name, address, phoneNumber);
-    }
-}
--- HolidayTravelVehicles/Salesperson.java
+++
@@ -1,6 +0,0 @@
-// ----------------------- Salesperson (InheritsPerson) ----------------------- //
-class Salesperson extends Person {
-    public Salesperson(String name, String address, String phoneNumber) {
-        super(name, address, phoneNumber);
-    }
-}
